d10115caa247a20a5038c7febf73f5768a7aea00
Rajan <rajanj448@student.sfit.ac.in>
2024-04-10 21:52:47 +0530

    Landing Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
          attribute="class"
          defaultTheme="dark"
          enableSystem
+         forcedTheme="dark"
         >
         {children}
         </ThemeProvider>
